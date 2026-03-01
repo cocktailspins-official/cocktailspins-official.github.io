4d1174ea3512e001f4fb39c5a541d0c2a714ecc0
cocktailspins-official <streamerstv27@gmail.com>
2026-03-01 02:39:17 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'cocktailspins-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/1000017793.jpg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
